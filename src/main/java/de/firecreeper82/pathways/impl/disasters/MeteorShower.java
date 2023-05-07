8699f1a0116d68b8f0ad2566c9e5d020ccfbd6a4
FireCreeper82 <85697504+FireCreeper82@users.noreply.github.com>
2023-05-07 21:12:04 +0200

    Demoness Pathway

--- src/main/java/de/firecreeper82/pathways/impl/disasters/MeteorShower.java
+++ src/main/java/de/firecreeper82/pathways/impl/disasters/MeteorShower.java
@@ -2,17 +2,13 @@ package de.firecreeper82.pathways.impl.disasters;
 
 import de.firecreeper82.lotm.Plugin;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.World;
-import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class MeteorShower extends Disaster{
@@ -44,18 +40,12 @@ public class MeteorShower extends Disaster{
                 @Override
                 public void run() {
                     startLoc.add(vector);
-                    spawnFallingBlocks(startLoc);
 
                     startLoc.add(vector);
                     world.spawnParticle(Particle.LAVA, startLoc, 35, 1, 1, 1, 0);
                     world.spawnParticle(Particle.SMOKE_NORMAL, startLoc, 35, 2, 2, 2, 0);
 
                     if(startLoc.getBlock().getType().isSolid()) {
-                        for(FallingBlock block : currentBlock) {
-                            block.setGravity(true);
-                            Random random = new Random();
-                            block.setVelocity(new Vector(random.nextInt(3), random.nextInt(3), random.nextInt(3)));
-                        }
                         startLoc.getWorld().createExplosion(startLoc, 30, true);
                         startLoc.getWorld().createExplosion(startLoc.clone().add(12, 0, 0), 40, true);
                         startLoc.getWorld().createExplosion(startLoc.clone().add(-12, 0, 0), 40, true);
@@ -66,27 +56,6 @@ public class MeteorShower extends Disaster{
                 }
             }.runTaskTimer(Plugin.instance, random.nextInt(25), 1);
         }
-    }
-
-    private final List<FallingBlock> currentBlock = new ArrayList<>();
-
-    public void spawnFallingBlocks(Location loc) {
-
-        for(FallingBlock block : currentBlock) {
-            block.remove();
-        }
-
-        Location startLoc = loc.clone();
-        World world = startLoc.getWorld();
-        if(world == null)
-            return;
-
-        Material mat;
-        mat = Material.MAGMA_BLOCK;
-        FallingBlock block = world.spawnFallingBlock(startLoc, mat.createBlockData());
-        block.setDropItem(false);
-        block.setGravity(false);
-        currentBlock.add(block);
     }
 
     @Override
